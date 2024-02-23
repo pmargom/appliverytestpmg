af655e0e9136c9e5fb621ed1b74f3798c0bcc697
Pedro Martin <pmargom@gmail.com>
2024-02-23 09:01:56 +0000

    Create XCode project

---
+++ MarvelAgent/MarvelAgent/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  MarvelAgent
+//
+//  Created by Pedro Martín Gómez on 23/2/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
