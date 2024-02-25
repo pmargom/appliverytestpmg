7dd6a213e304d950e5f2d981b36b65aa87a5ad25
Pedro Martin <pmargom@gmail.com>
2024-02-25 14:21:48 +0000

    Show the rest of the fields of the character

--- MarvelAgent/MarvelAgent/Presentation/UI/CharacterDetailsView.swift
+++ MarvelAgent/MarvelAgent/Presentation/UI/CharacterDetailsView.swift
@@ -9,10 +9,39 @@ import SwiftUI
 
 struct CharacterDetailsView: View {
     let characterItem: CharacterListItem
-
-    var body: some View {
-        Text("Selected \(characterItem.name)")
-            .font(.largeTitle)
+    
+    var body: some View {    
+        VStack() {
+            HStack {
+                AsyncImage(url: URL(string: characterItem.image)!) { image in
+                    image
+                        .resizable()
+                        .foregroundColor(.accentColor)
+                }
+            placeholder: {
+                ProgressView()
+            }
+            .frame(width: 200, height: 160)
+                
+                VStack(alignment: .leading) {
+                    Text(characterItem.name)
+                        .font(.largeTitle)
+                        .padding()
+                    
+                    Text(characterItem.gender)
+                        .font(.largeTitle)
+                        .padding()
+                    
+                    Text(characterItem.species)
+                        .font(.largeTitle)
+                        .padding()
+                }
+                Spacer()
+            }
             .padding()
+            
+            Spacer()
+        }
+        .edgesIgnoringSafeArea(.all)
     }
 }
