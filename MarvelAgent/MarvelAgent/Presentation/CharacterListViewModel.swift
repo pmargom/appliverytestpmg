b85b55098877d978bf76fcf769cf7c0d9cf46900
Pedro Martin <pmargom@gmail.com>
2024-02-25 08:37:28 +0000

    Update viewmodel

--- MarvelAgent/MarvelAgent/Presentation/CharacterListViewModel.swift
+++ MarvelAgent/MarvelAgent/Presentation/CharacterListViewModel.swift
@@ -27,7 +27,6 @@ struct CharacterListItem {
 class CharacterListViewModel: ObservableObject {
     private let getCharacterListUseCase: GetCharacterListUseCaseProtocol
     @Published var characterItems: [CharacterListItem] = []
-    @Published var items: [Item] = []
     
     init(getCharacterListUseCase: GetCharacterListUseCaseProtocol) {
         self.getCharacterListUseCase = getCharacterListUseCase
@@ -44,35 +43,4 @@ class CharacterListViewModel: ObservableObject {
             characterItems = characterEntityList.map { CharacterListItem(characterEntity: $0) }
         }
     }
-
-    
-//    func onAppear() {
-//        Task {
-//            let result = await getCharacterListUseCase.execute()
-//            let characterEntityList = try? result.get()
-//            guard let characterEntityList else {
-//                return
-//            }
-//
-//            characterEntities = characterEntityList.map { CharacterListItem(characterEntity: $0) }
-//        }
-//    }
-    
-    func loadData() async {
-        // Simulamos un retraso para la carga de datos
-        try? await Task.sleep(nanoseconds: 2_000_000_000) // 2 segundos
-        
-        // Actualizamos los items
-        items = [
-            Item(id: 1, name: "Item 1 cargado"),
-            Item(id: 2, name: "Item 2 cargado"),
-            Item(id: 3, name: "Item 3 cargado")
-        ]
-    }
 }
-
-struct Item: Identifiable {
-    let id: Int
-    let name: String
-}
-
---
+++ MarvelAgent/MarvelAgent/Presentation/UI/CharacterDetailsView.swift
@@ -0,0 +1,8 @@
+//
+//  CharacterDetailsView.swift
+//  MarvelAgent
+//
+//  Created by Pedro Martín Gómez on 25/2/24.
+//
+
+import Foundation
